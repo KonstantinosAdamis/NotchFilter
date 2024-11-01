ff55ad08590cfc9d443b2c953055beda83feebca
Konstantinos Adamis <137218524+KonstantinosAdamis@users.noreply.github.com>
2024-11-01 15:31:57 +0200

    Update notch filter.py

--- notch filter.py
+++ notch filter.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 # Constants
 R = 8e-3  # Resistance term in denominator
-L = 90.3e-12  # Inductance term in denominator
+C = 90.3e-12  # Capacitance term in denominator
 numerator_gain = 16e-6  # Gain in numerator
 
 # Frequency to notch (in Hz)
@@ -13,7 +13,7 @@ omega_0 = 2 * np.pi * f_notch  # Angular frequency for notch
 
 # Define the transfer function components
 numerator = [1, 0, omega_0**2]  # s^2 + ω₀^2
-denominator = [R * L, R, 0]  # (R*L)s + R
+denominator = [R * C, R, 0]  # (R*C)s + R
 
 # Construct transfer function
 system = signal.TransferFunction(numerator, denominator)
